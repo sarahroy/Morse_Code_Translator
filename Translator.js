1110d1e1cf224f8a92aaa52cda386dfe5abb9310
Sarah Roy <sarah2000roy@gmail.com>
2023-09-04 17:42:48 -0400

    updating files

--- Translator.js
+++ Translator.js
@@ -1,28 +1,28 @@
 //Author: Sarah Ann Roy
 //Project Name: MorseCodeTranslator
-//Project Description: A program written in JavaScript to be able to encrypt English phrases into Morse Code and decrypt Morse Code into English.
+//Project Description: A program written in JavaScript to be able to encode English phrases into Morse Code and decode Morse Code into English.
 //InternationalMorseCode.png source: https://scoutlife.org/hobbies-projects/funstuff/575/morse-code-translator/ 
 
 import promptSync from 'prompt-sync'; //importing the synthetic default from prompt-sync
-import { decrypt } from './decrypt.js'; //importing decrypt function
-import { encrypt } from './encrypt.js'; //importing encrypt function
+import { decode } from './decode.js'; //importing decode function
+import { encode } from './encode.js'; //importing encode function
 const prompt = promptSync();
 
-console.log("Enter 0 to encrypt English phrases => Morse Code");
-console.log("Enter 1 to decrypt Morse Code => English");
+console.log("Enter 0 to encode English phrases => Morse Code");
+console.log("Enter 1 to decode Morse Code => English");
 var choice = parseInt(prompt("Enter you choice: "));//take user input as int
 
 if(choice == 0) // English -> Morse Code
 {
     console.log("English -> Morse Code");
-    var phrase = prompt("Enter English phrase to be encrypted: "); //prompting user to input phrase
-    console.log("Morse Code: "+encrypt(phrase)); //passing user inputted string to encrypt function
+    var phrase = prompt("Enter English phrase to be encoded: "); //prompting user to input phrase
+    console.log("Morse Code: "+encode(phrase)); //passing user inputted string to encode function
 }
 else if(choice == 1) // Morse Code -> English
 {
     console.log("Morse Code -> English");
-    var code = prompt("Enter Morse Code to be decrypted: "); //prompting user to input morse code
-    console.log("Translated Message: "+decrypt(code)); //passing user inputted string to decrypt function
+    var code = prompt("Enter Morse Code to be decodeed: "); //prompting user to input morse code
+    console.log("Translated Message: "+decode(code)); //passing user inputted string to decode function
 }
 else //user input was neither 0 nor 1
 {
